b1190a20138a4109d272c019a2e6bfb983aea4a5
tom <aa123593465@gmail.com>
2023-12-24 11:03:29 +0800

    backend: schema, api setup.

---
+++ final/src/app/api/journeys/[jid]/route.ts
@@ -0,0 +1,114 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+import { and, eq } from "drizzle-orm";
+import Pusher from "pusher";
+
+import { db } from "@/db";
+import { journeysTable, travelCardsTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+import { privateEnv } from "@/lib/env/private";
+import { publicEnv } from "@/lib/env/public";
+
+// DELETE /api/journeys/:jid
+// Delete a journey.
+export async function DELETE(
+  req: NextRequest,
+  {
+    params,
+  }: {
+    params: {
+      jid: string;
+    };
+  },
+) {
+
+  try {
+    const journeyId = params.jid;
+    const session = await auth();
+    if (!session || !session?.user?.id) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const [journey] = await db
+      .select({
+        displayId: journeysTable.displayId,
+      })
+      .from(journeysTable)
+      .where(and(eq(journeysTable.displayId, journeyId)));
+
+    if (!journey) {
+      return NextResponse.json({ error: "Journey not found" }, { status: 404 });
+    }
+
+    const result = await db
+      .delete(journeysTable)
+      .where(and(eq(journeysTable.displayId, journeyId)))
+      .execute();
+
+    if (!result) {
+      return NextResponse.json({ error: "Journey not found" }, { status: 404 });
+    }
+
+    return NextResponse.next();
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
+
+
+
+// PUT /api/journeys/:jid
+// Update a journey.
+export async function PUT(
+  req: NextRequest,
+  {
+    params,
+  }: {
+    params: {
+      jid: string;
+    };
+  },
+)
+{
+  try {
+    const journeyId = params.jid;
+    const session = await auth();
+    if (!session || !session?.user?.id) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const [journey] = await db
+      .select({
+        displayId: journeysTable.displayId,
+      })
+      .from(journeysTable)
+      .where(and(eq(journeysTable.displayId, journeyId)));
+
+    if (!journey) {
+      return NextResponse.json({ error: "Journey not found" }, { status: 404 });
+    }
+    const setVal = await req.json();
+    
+    const result = await db
+      .update(journeysTable)
+      .set(setVal)
+      .where(and(eq(journeysTable.displayId, journeyId)))
+      .execute();
+
+    if (!result) {
+      return NextResponse.json({ error: "Journey not found" }, { status: 404 });
+    }
+
+    return NextResponse.next();
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
---
+++ final/src/app/api/journeys/route.ts
@@ -0,0 +1,68 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+import { and, eq } from "drizzle-orm";
+import Pusher from "pusher";
+
+import { db } from "@/db";
+import { journeysTable, travelCardsTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+import { privateEnv } from "@/lib/env/private";
+import { publicEnv } from "@/lib/env/public";
+
+// POST /api/journeys
+// Create a new journey.
+export async function POST(req: NextRequest) {
+  try {
+    const session = await auth();
+    if (!session || !session.user?.id) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const userId = session.user.id;
+    const { travelCardsId, title, note, location, date, time1, time2 } =
+      await req.json();
+
+    // create journey
+    const [res] = await db
+      .insert(journeysTable)
+      .values({
+        title: title,
+        note: note,
+        location: location,
+        date: date,
+        time1: time1,
+        time2: time2,
+        travelCardsId: travelCardsId,
+      })
+      .returning()
+      .execute();
+
+    // pusher
+    const pusher = new Pusher({
+      appId: privateEnv.PUSHER_ID,
+      key: publicEnv.NEXT_PUBLIC_PUSHER_KEY,
+      secret: privateEnv.PUSHER_SECRET,
+      cluster: publicEnv.NEXT_PUBLIC_PUSHER_CLUSTER,
+      useTLS: true,
+    });
+
+    await pusher.trigger(`private-${userId}`, "journey:update", {
+      senderId: userId,
+    });
+
+    return NextResponse.json(
+      {
+        message: res,
+      },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
+
+// PUT /api/journeys/
