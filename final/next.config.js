b7172ad4dc1f9d2ee73505e8039f86e3fa6d297b
tom <aa123593465@gmail.com>
2023-12-30 14:58:23 +0800

    web final complete3

--- final/next.config.js
+++ final/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        staticPageGenerationTimeout: 300,
+    },
+    
+}
 
 module.exports = nextConfig;
